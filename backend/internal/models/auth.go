e1d5ee12dd4a4e0bb3370f214fdd46d0b3592ffb
hajji-Ismail <hajji.ismail.851@gmail.com>
2025-06-11 13:30:34 +0100

    adding somme new feild to the user database and check for empty feilds in the backend

--- backend/internal/models/auth.go
+++ backend/internal/models/auth.go
@@ -3,7 +3,11 @@ package models
 
 type User struct {
 	ID  int  `json:"id"`
-	Nickname string `json:nickname`
-	Email string `json:email`
+	Nickname string `json:"nickname"`
+	Email string `json:"email"`
 	PassWord string `json:"password"`
+	FirstName string `json:"firstname"`
+	Lastname string `json:"lastname"`
+	DateofBirth string `json:"dateofbirth"`
+	AboutMe  string `json:"aboutme"`
 }
--- backend/internal/services/auth/auth_service.go
+++ backend/internal/services/auth/auth_service.go
@@ -18,7 +18,37 @@ func NewAuthService(Authrepo *repositories.AuthRepository) *AuthService {
 func (s *AuthService) SaveUser(user *models.User) error {
 	if len(user.Nickname) < 3 || len(user.Nickname) > 20 {
 		return fmt.Errorf("Nickname must be betwwen 3 and 20 charachter")
+
+	}
+	if err:= s.CheckEmpty(user);err!=nil {
+		return err
 	}
 
+
+
+
 	return nil
+}
+func (s *AuthService) CheckEmpty(user *models.User)error{
+
+	if user.DateofBirth == "" {
+	return fmt.Errorf("Date Of Birth Is Empty")
+	}
+		if user.Email == "" {
+	return fmt.Errorf("Email Is Empty")
+	}
+		if user.FirstName == "" {
+	return fmt.Errorf("First Name Is Empty")
+	}
+		if user.Lastname == "" {
+	return fmt.Errorf("Last Name Is Empty")
+	}
+		if user.PassWord == "" {
+	return fmt.Errorf("PassWord Is Empty")
+	}
+
+
+	return nil 
+
+	
 }

1036b1ce4b116ceb2f895e3d8d502e750bb39578
helouaziz <ouazizi2code@gmail.com>
2025-06-16 11:31:18 +0100

    implement PostRepository structure and methods

--- backend/internal/repositories/post/post_repository.go
+++ backend/internal/repositories/post/post_repository.go
@@ -1,1 +1,18 @@
 package repositories
+
+import (
+	"database/sql"
+
+	"github.com/ismailsayen/social-network/internal/models"
+)
+
+type PostRepository struct {
+	db *sql.DB
+}
+
+func NewPostRepo(db *sql.DB) *PostRepository {
+	return &PostRepository{db: db}
+}
+
+func (r *PostRepository) SavePost(user *models.User) {
+}

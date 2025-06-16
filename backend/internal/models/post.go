23e52e3db32df24923aeaae272ff5db991ec2412
helouaziz <ouazizi2code@gmail.com>
2025-06-16 12:57:55 +0100

    refactor: update Post struct to change Media type from *string to []byte and remove Comments field

--- backend/internal/models/post.go
+++ backend/internal/models/post.go
@@ -1,14 +1,14 @@
 package models
 
 type Post struct {
-	ID       int     `json:"id"`
-	UserID   int     `json:"user_id"`
-	Title    string  `json:"title"`
-	Content  string  `json:"body"`
-	Media    *string `json:"media,omitempty"`
-	Likes    int     `json:"likes"`
-	Dislikes int     `json:"dislikes"`
-	Comments int     `json:"comments"`
-	Privacy string `json:"privacy"` 
+	ID        int    `json:"id"`
+	UserID    int    `json:"user_id"`
+	Title     string `json:"title"`
+	Content   string `json:"body"`
+	Media     []byte `json:"media,omitempty"`
+	Likes     int    `json:"likes"`
+	Dislikes  int    `json:"dislikes"`
+	// Comments  int    `json:"comments"`
+	Privacy   string `json:"privacy"`
 	CreatedAt string `json:"created_at"`
 }

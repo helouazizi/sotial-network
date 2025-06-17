faa41b534cabd5399e3bd2d640d562bb5b299e62
yelasri <elasriyoussef604@gmail.com>
2025-06-17 11:57:25 +0100

    chat

--- backend/internal/handlers/chat/chat_handler.go
+++ backend/internal/handlers/chat/chat_handler.go
@@ -1,1 +1,19 @@
 package handlers
+
+import (
+	"net/http"
+
+	"github.com/gorilla/websocket"
+	services "github.com/ismailsayen/social-network/internal/services/chat"
+)
+
+type ChatHandler struct {
+	service *services.ChatService
+}
+
+// Upgrader is used to upgrade HTTP connections to WebSocket connections.
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}

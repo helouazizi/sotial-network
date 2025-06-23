21c5481bb14ee5b90389821ee05b6bbb17da216f
Ismail Sayen <ismailsvn02@gmail.com>
2025-06-23 15:50:21 +0100

    add context profile an handle myProfile

--- frontend/app/components/profile/Visibility.tsx
+++ frontend/app/components/profile/Visibility.tsx
@@ -1,19 +1,33 @@
 import { useProfile } from '@/app/context/ProfileContext'
 import React from 'react'
-import { FaLock, FaUnlock } from 'react-icons/fa'
 
 const Visibility = () => {
     const { dataProfile, setDataProfile } = useProfile()
+    const change = () => {
+
+        setDataProfile((prev) => {
+            if (!prev) return prev;
+            return {
+                ...prev,
+                is_private: prev.is_private === 1 ? 0 : 1,
+            };
+        });
+    };
     return (
         <>
             {
                 dataProfile?.myAccount ?
-                    <label className="switch">
-                        <input type="checkbox" />
-                        <span className="slider"></span>
-                    </label> :
+                    <>
+                        <label className="switch" >
+                            <input type="checkbox" checked={dataProfile?.is_private == 1 ? true : false} onChange={change} />
+                            <span className="slider"></span>
+                        </label>
+                        {dataProfile?.is_private == 1 ? <p>switch to PUBLIC</p> : <p>switch to PRIVATE</p>}
+                    </>
+                    :
                     ""
             }
+
         </>
     )
 }

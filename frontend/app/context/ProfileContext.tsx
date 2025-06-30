fd9218cb7835e10219ac149905da3370876853cf
Ismail Sayen <ismailsvn02@gmail.com>
2025-06-30 14:52:23 +0100

    handle navigation of profile

--- frontend/app/context/ProfileContext.tsx
+++ frontend/app/context/ProfileContext.tsx
@@ -4,15 +4,26 @@ import React, { createContext, useContext, useEffect, useState } from "react";
 import { ProfileInt } from "../types/profiles";
 
 interface ProfileContextType {
-    dataProfile: ProfileInt | null
-    setDataProfile: React.Dispatch<React.SetStateAction<ProfileInt | null>>
+    dataProfile: ProfileInt | null;
+    setDataProfile: React.Dispatch<React.SetStateAction<ProfileInt | null>>;
+    loading: boolean;
 }
+
 const ProfileContext = createContext<ProfileContextType | undefined>(undefined);
 
-export const ProfileProvider = ({ children, profileId }: { children: React.ReactNode; profileId: string }) => {
-    const [dataProfile, setDataProfile] = useState<ProfileInt | null>(null)
+export const ProfileProvider = ({
+    children,
+    profileId,
+}: {
+    children: React.ReactNode;
+    profileId: string;
+}) => {
+    const [dataProfile, setDataProfile] = useState<ProfileInt | null>(null);
+    const [loading, setLoading] = useState(true);
+
     useEffect(() => {
         const fetchProfile = async () => {
+            setLoading(true);
             try {
                 const res = await fetch(
                     `http://localhost:8080/api/v1/profile?id=${profileId}`,
@@ -24,18 +35,22 @@ export const ProfileProvider = ({ children, profileId }: { children: React.React
                 const data: ProfileInt = await res.json();
                 setDataProfile(data);
             } catch (err) {
-                console.error("Failed to fetch profprofileile:", err);
+                console.error("Failed to fetch profile:", err);
+            } finally {
+                setLoading(false);
             }
         };
 
         fetchProfile();
     }, [profileId]);
+
     return (
-        <ProfileContext.Provider value={{ dataProfile, setDataProfile }}>
+        <ProfileContext.Provider value={{ dataProfile, setDataProfile, loading }}>
             {children}
         </ProfileContext.Provider>
-    )
-}
+    );
+};
+
 export const useProfile = () => {
     const context = useContext(ProfileContext);
     if (!context) {

a37935e832fc7a652037cf32554168161651138e
Ismail Sayen <ismailsvn02@gmail.com>
2025-07-05 18:51:24 +0100

    handle Relations css and fix bug of duplicated data

--- frontend/components/request/ToogleInitiale.tsx
+++ frontend/components/request/ToogleInitiale.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { MdOutlineArrowRight } from 'react-icons/md'
 
 const ToogleInitiale = (props: { showToggle: boolean, setShowToggle: Function }) => {
-
-
+    const [activePage, SetActivePage] = useState('follow')
+    const CloseToogle = () => {
+        props.setShowToggle(false)
+    }
+    const ChangePage = (page: string) => {
+        SetActivePage(page)
+    }
     return (
-        <div className={`ToogleInitiale ${props.showToggle ? "show" : "hide"}`} >ToogleInitiale</div>
+        <div className={`ToogleInitiale ${props.showToggle ? "show" : "hide"}`} >
+
+            <div className="close-toogle">
+                <MdOutlineArrowRight onClick={CloseToogle} />
+            </div>
+            <div className="navigationToogle">
+                <button className={`${activePage == "follow" ? 'active-request' : ""}`} onClick={() => ChangePage("follow")}>FOLLOW (8)</button>
+                <button className={`${activePage == "groupe" ? 'active-request' : ""}`} onClick={() => ChangePage("groupe")}>GROUPE (1)</button>
+            </div>
+        </div>
     )
 }
 

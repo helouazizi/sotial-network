8027c5847f206034d5e0b09d545a2e5b05e47277
helouaziz <ouazizi2code@gmail.com>
2025-07-12 11:51:48 +0100

    qdd groups lqyout

---
+++ frontend/app/groups/joined/[id]/layout.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import React, { ReactNode } from 'react'
+import SwitchButtons from '@/components/groups/SwitchButtons'
+import { useParams, usePathname } from 'next/navigation';
+
+
+function GroupPageLayout({ children }: { children: ReactNode }) {
+  const url = usePathname()
+  const {id} = useParams()
+  console.log(url ,"url");
+  
+  return (
+    <div className='group-page'>
+      <SwitchButtons firstButtonContent='Posts' secondButtonContent='Events' firstButtonLink={`${url}`} secondButtonLink={`/groups/joined/${id}/events`} />
+      <div>
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default GroupPageLayout

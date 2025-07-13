05a841b2b10ebd079dcf9363026c3b4a483ddd9e
Ismail Sayen <ismailsvn02@gmail.com>
2025-07-13 18:46:36 +0100

    style chat group

--- frontend/components/groups/GroupChatHeader.tsx
+++ frontend/components/groups/GroupChatHeader.tsx
@@ -1,15 +1,18 @@
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import { title } from 'process'
 import React from 'react'
+import { HiUserGroup } from 'react-icons/hi'
 import { MdGroups } from 'react-icons/md'
 
 const GroupChatHeader = (props: { idGrp: number | undefined, title: string | undefined, members: number | undefined }) => {
     let pathName = usePathname()
     let title = props.title && props.title.length > 25 ? props.title.slice(0, 25).trim() + "..." : props.title
-    let path = pathName.startsWith("/chat/groupsChat") ? "/groups/joined/" + props.idGrp +"/posts": ""
+    let path = pathName.startsWith("/chat/groupsChat") ? "/groups/joined/" + props.idGrp + "/posts" : ""
     return (
-        <div className='chat-grp-header'><Link href={path}><span><MdGroups /></span> <p>{title}</p></Link></div>
+        <div className='chat-grp-header'>
+            <Link href={path}><span><MdGroups /></span> <p>{title}</p></Link>
+            <div className='members'><span>{props.members}</span> <HiUserGroup /> </div>
+        </div>
     )
 }
 

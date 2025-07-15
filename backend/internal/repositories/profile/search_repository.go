8c04e92017a7643f0f7886108727c44a98335515
hajji-Ismail <hajji.ismail.851@gmail.com>
2025-07-15 18:03:56 +0100

    Implement search functionality with SearchInput component and style updates

--- backend/internal/repositories/profile/search_repository.go
+++ backend/internal/repositories/profile/search_repository.go
@@ -4,7 +4,7 @@ import "github.com/ismailsayen/social-network/internal/models"
 
 func (repo *ProfileRepository)SerchProfile(data string) ([]models.User, error) {
 	
-	query :=`SELECT last_name, first_name,  nickname, avatar FROM users
+	query :=`SELECT id, last_name, first_name,  nickname, avatar FROM users
 WHERE LOWER(first_name) LIKE $1
    OR LOWER(last_name) LIKE $1
    OR LOWER(nickname) LIKE $1
@@ -16,7 +16,7 @@ WHERE LOWER(first_name) LIKE $1
    }
    for rows.Next(){
 	var user  models.User
-	err := rows.Scan( &user.Lastname, &user.FirstName,&user.Nickname, &user.Avatar)
+	err := rows.Scan( &user.ID ,&user.Lastname, &user.FirstName,&user.Nickname, &user.Avatar)
 	if err != nil {
 		return  []models.User{}, err
 	}

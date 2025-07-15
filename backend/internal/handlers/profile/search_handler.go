67be808d812fb8c5d2681cf6e7d58a8e9299b730
hajji-Ismail <hajji.ismail.851@gmail.com>
2025-07-15 16:32:13 +0100

    Add SearchProfile functionality with handler and repository integration

---
+++ backend/internal/handlers/profile/search_handler.go
@@ -0,0 +1,40 @@
+package profile
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/ismailsayen/social-network/pkg/utils"
+)
+
+func (h *ProfileHandler) SearchProfileHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		utils.ResponseJSON(w, http.StatusMethodNotAllowed, map[string]any{
+			"message": "Method not allowed",
+			"status":  http.StatusMethodNotAllowed,
+		})
+		return
+	}
+	query := strings.TrimSpace(r.URL.Query().Get("message"))
+	if query == "" {
+		utils.ResponseJSON(w, http.StatusBadRequest, map[string]any{
+			"message": "Search query is required",
+			"status":  http.StatusBadRequest,
+		})
+		return
+	}
+	users, err := h.service.SearchProfile(query)
+	if err != nil {
+		fmt.Println(err)
+		utils.ResponseJSON(w, http.StatusInternalServerError, map[string]any{
+			"message": "Internal Server Error",
+			"status":  http.StatusInternalServerError,
+		})
+		return
+	}
+	utils.ResponseJSON(w, http.StatusOK, map[string]any{
+		"message": users,
+		"status":  http.StatusOK,
+	})
+}
---
+++ backend/internal/repositories/profile/search_repository.go
@@ -0,0 +1,30 @@
+package profile
+
+import "github.com/ismailsayen/social-network/internal/models"
+
+func (repo *ProfileRepository)SerchProfile(data string) ([]models.User, error) {
+	
+	query :=`SELECT last_name, first_name,  nickname, avatar FROM users
+WHERE LOWER(first_name) LIKE $1
+   OR LOWER(last_name) LIKE $1
+   OR LOWER(nickname) LIKE $1
+   `
+   var users []models.User 
+   rows, err := repo.db.Query(query,data)
+   if err != nil {
+	return []models.User{}, err
+   }
+   for rows.Next(){
+	var user  models.User
+	err := rows.Scan( &user.Lastname, &user.FirstName,&user.Nickname, &user.Avatar)
+	if err != nil {
+		return  []models.User{}, err
+	}
+	users= append(users, user)
+
+
+   }
+
+	return users, nil
+
+}

6fcdc7ef8f9b9ce6a5842c0270f92216feb436a4
hajji-Ismail <hajji.ismail.851@gmail.com>
2025-07-16 13:20:47 +0100

    feat(NotFound): add Not Found page and styles style(globals): import Not Found styles refactor(middleware): enhance route handling and error logging refactor(header): remove unnecessary display property from profile wrapper

---
+++ frontend/app/not-found.tsx
@@ -0,0 +1,12 @@
+// app/groups/joined/[id]/posts/not-found.tsx
+
+export default function NotFound() {
+  return (
+  <div className="center-container">
+  <div className="content">
+    <h1 className="title">Post Not Found</h1>
+    <p className="subtitle">We couldn’t find the post you’re looking for.</p>
+  </div>
+</div>
+  );
+}

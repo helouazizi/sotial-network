811d284667be7ecc65284da3489c31fc3977d84e
hajji-Ismail <hajji.ismail.851@gmail.com>
2025-07-16 10:50:43 +0100

    Refactor SearchInput component for improved readability and structure

--- frontend/components/search/search.tsx
+++ frontend/components/search/search.tsx
@@ -1,36 +1,36 @@
-import { PopupContext } from "@/context/PopupContext"
-import { API_URL } from "@/services"
-import { useContext, useEffect, useRef, useState } from "react"
-import { User } from "@/types/user"
-import PostHeader from "../post/postHeader"
-import AboutProfileUser from "../profile/AboutProfileUser"
-import Link from "next/link"
+import { PopupContext } from "@/context/PopupContext";
+import { API_URL } from "@/services";
+import { useContext, useEffect, useRef, useState } from "react";
+import { User } from "@/types/user";
+import PostHeader from "../post/postHeader";
+import Link from "next/link";
 
-export  const SearchInput = ()=>{
-    const popup = useContext(PopupContext)
-        const [data , setData] = useState('')
-    const [users , setUsers] = useState<User[]>([])
-     const typingTimeoutRef = useRef<NodeJS.Timeout | null>(null);
-    const searchHandling = async ()=>{
-        try {
-            const res = await fetch(`${API_URL}api/v1/SearchProfile?message=${data}`, {
-                method : 'GET',
-                credentials : 'include'
-
-            })
-            if (!res.ok){
-                throw new Error("Something went wrong");
-            }
-            const result = await res.json()
-            console.log(result.message, '--------------------------result');
-            
-            setUsers(result.message)
-
-            
-        } catch (error : any) {
-            popup?.showPopup("faild", error.message)
+export const SearchInput = () => {
+  const popup = useContext(PopupContext);
+  const [data, setData] = useState("");
+  const [users, setUsers] = useState<User[]>([]);
+  const [isEmpty, setEMPty] = useState<boolean>(false);
+  const typingTimeoutRef = useRef<NodeJS.Timeout | null>(null);
+  const searchHandling = async () => {
+    try {
+      const res = await fetch(
+        `${API_URL}api/v1/SearchProfile?message=${data}`,
+        {
+          method: "GET",
+          credentials: "include",
         }
+      );
+      if (!res.ok) {
+        throw new Error("Something went wrong");
+      }
+      const result = await res.json();
+      console.log(result.message, "--------------------------result");
+
+      setUsers(result.message);
+    } catch (error: any) {
+      popup?.showPopup("faild", error.message);
     }
+  };
   const handleTyping = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
 
@@ -44,56 +44,56 @@ export  const SearchInput = ()=>{
       setData(value);
     }, 500);
   };
-  useEffect(()=>{
-     if (data !== '') {
+  useEffect(() => {
+    if (data !== "") {
       searchHandling();
-    } 
+      setEMPty(false)
+    }else{
+      setEMPty(true)
+    }
+  }, [data]);
+  return (
+    <>
+      <div className="Searching">
+        <input type="text" placeholder="Search..." onChange={handleTyping} />
 
-    
-  }, [data])
- return (
-  <>
-    <div className="Searching">
-      <input
-        type="text"
-        placeholder="Search..."
-        onChange={handleTyping}
-      />
-
-      {/* Wrap the results */}
-      {users && users.length > 0 && (
-        <div className="search-results">
-          {users.map((user, index) => {
-            if (!data) return null; 
-            return (
-              <Link key={index} href={`/profile/${user?.id}`} className="block">
-                <div className="cursor-pointer">
-                  {user?.avatar && (
-                    <img
-                      src={`http://localhost:8080/images/user/${user?.avatar}`}
-                      alt={user?.nickname}
-                      className="avatar-profile header-icon"
+        {/* Wrap the results */}
+        {users && users.length > 0 && (
+          <div className="search-results">
+            {users.map((user, index) => {
+              if (isEmpty) return null;
+              return (
+                <Link
+                  key={index}
+                  href={`/profile/${user?.id}`}
+                  className="block"
+                >
+                  <div className="cursor-pointer" onClick={()=>{setEMPty(true)}}>
+                    {user?.avatar && (
+                      <img
+                        src={`http://localhost:8080/images/user/${user?.avatar}`}
+                        alt={user?.nickname}
+                        className="avatar-profile header-icon"
+                      />
+                    )}
+                    <PostHeader
+                      author={
+                        user.nickname
+                          ? user.nickname
+                          : `${user.firstname}-${user.lastname}`
+                      }
+                      firstname={user.firstname}
+                      lastname={user.lastname}
+                      createdAt=""
+                      avatarUrl={user.avatar}
                     />
-                  )}
-                  <PostHeader
-                    author={
-                      user.nickname
-                        ? user.nickname
-                        : `${user.firstname}-${user.lastname}`
-                    }
-                    firstname={user.firstname}
-                    lastname={user.lastname}
-                    createdAt=""
-                    avatarUrl={user.avatar}
-                  />
-                </div>
-              </Link>
-            );
-          })}
-        </div>
-      )}
-    </div>
-  </>
-);
-
-}
+                  </div>
+                </Link>
+              );
+            })}
+          </div>
+        )}
+      </div>
+    </>
+  );
+};

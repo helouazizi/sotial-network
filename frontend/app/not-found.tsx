99e720f000355f2e8021a9893b1104ea9e932773
hajji-Ismail <hajji.ismail.851@gmail.com>
2025-07-16 13:35:46 +0100

    fix(NotFound): update Not Found page title and message

--- frontend/app/not-found.tsx
+++ frontend/app/not-found.tsx
@@ -4,8 +4,8 @@ export default function NotFound() {
   return (
   <div className="center-container">
   <div className="content">
-    <h1 className="title">Post Not Found</h1>
-    <p className="subtitle">We couldn’t find the post you’re looking for.</p>
+    <h1 className="title"> 404</h1>
+    <p className="subtitle">Page Not Found.</p>
   </div>
 </div>
   );

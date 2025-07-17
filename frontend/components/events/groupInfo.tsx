f07ea0f76d1478354e129743faa7af4a5b0b6545
hajji-Ismail <hajji.ismail.851@gmail.com>
2025-07-17 11:48:56 +0100

    Refactor GroupHeader component for improved functionality

--- frontend/components/events/groupInfo.tsx
+++ frontend/components/events/groupInfo.tsx
@@ -1,247 +1,259 @@
-'use client'
+"use client";
 
 import { MdGroups, MdVerifiedUser } from "react-icons/md";
 import { GetGroup, SendJoinGroupRequest } from "@/services/groupServices";
 import { useContext, useEffect, useState } from "react";
 import { GroupInfo, GroupMembers } from "@/types/events";
 import FormatDate from "@/utils/date";
-import { GetGroupMembers, GetUnrequestedFolowers } from "@/services/eventsServices";
+import {
+  GetGroupMembers,
+  GetUnrequestedFolowers,
+} from "@/services/eventsServices";
 import PostHeader from "../post/postHeader";
 import { Follower } from "@/types/post";
 import { PopupContext } from "@/context/PopupContext";
 import { GroupNotifications } from "@/types/Request";
+import { useRouter } from "next/navigation";
 
 function GroupHeader({ id }: { id: number }) {
-    const [groupInfo, setGroupInfo] = useState<GroupInfo | null>(null);
-    const poopUp = useContext(PopupContext)
-    const [groupMembers, setGroupMembers] = useState<GroupMembers | null>(null);
-    const [showMembers, setShowMembers] = useState(false);
-    const [showFolowers, setShowFolowers] = useState(false);
-    const [followers, setFollowers] = useState<Follower[]>([]);
-    const [invited, setInvited] = useState<number[]>([]);
-    const [isSending, setIsSending] = useState(false);
+  const [groupInfo, setGroupInfo] = useState<GroupInfo | null>(null);
+  const poopUp = useContext(PopupContext);
+  const [groupMembers, setGroupMembers] = useState<GroupMembers | null>(null);
+  const [showMembers, setShowMembers] = useState(false);
+  const [showFolowers, setShowFolowers] = useState(false);
+  const [followers, setFollowers] = useState<Follower[]>([]);
+  const [invited, setInvited] = useState<number[]>([]);
+  const [isSending, setIsSending] = useState(false);
+  const  router = useRouter()
 
-    const handleToggleFollower = (id: number, checked: boolean) => {
-        setInvited((prev) =>
-            checked ? [...prev, id] : prev.filter((uid) => uid !== id)
-        );
-
-    };
-
-
-
-    const displayMembers = () => {
-        return (
-            <div className="share-with-users">
-                <label className="share-with-label">
-                    Gorup members
-                </label>
-                <ul className="user-checkbox-list">
-                    {groupMembers && groupMembers.members.map((member, indx) => (
-                        <li key={indx} className="event-member-item">
-                            <PostHeader author={member.nickname} avatarUrl={member.avatar} firstname={member.firstname} lastname={member.lastname} createdAt="" />
-
-                        </li>
-                    ))
-                    }
-                </ul>
-
-            </div>
-        )
-    }
-
-    const displayFolowers = () => {
-
-        return (
-            <div className="share-with-users">
-                <label className="share-with-label">
-                    Invite specific followers
-                </label>
-                <ul className="user-checkbox-list">
-                    {followers && followers.length > 0 && followers.map((f, index) => (
-                        <li key={index} className="user-checkbox-item">
-                            <label id="follower-checkbox-label">
-                                <PostHeader
-                                    author={`${f.User?.firstname} ${f.User?.lastname}`}
-                                    firstname={f.User?.firstname} lastname={f.User?.lastname}
-                                    createdAt="" avatarUrl={f.User?.avatar}
-                                />
-                                <input
-                                    type="checkbox"
-                                    checked={invited.includes(f.User?.id)}
-                                    onChange={(e) =>
-                                        handleToggleFollower(f.User?.id, e.target.checked)
-                                    }
-                                    title={`Invite ${f.User?.firstname} ${f.User?.lastname}`}
-                                    placeholder={`Select follower ${f.User?.firstname} ${f.User?.lastname}`}
-                                    aria-label={`Invite ${f.User?.firstname} ${f.User?.lastname}`}
-                                />
-                            </label>
-                        </li>
-
-                    ))}
-                </ul>
-            </div>
-        )
-    }
-
-    useEffect(() => {
-        const fetchGroup = async () => {
-            try {
-                const data = await GetGroup(id);
-                setGroupInfo(data);
-            } catch (err) {
-                poopUp?.showPopup("faild", "something went wrong, please try again")
-            }
-        };
-
-        fetchGroup();
-    }, [id]);
-
-
-    const fetchMembers = async () => {
-        try {
-            const data = await GetGroupMembers(id);
-            setGroupMembers(data);
-        } catch (err) {
-            poopUp?.showPopup("faild", "something went wrong, please try again")
-        }
-    };
-
-
-
-    const fetchFolowers = async () => {
-        try {
-            const data = await GetUnrequestedFolowers(id);
-            setFollowers(data)
-
-        } catch (err) {
-            poopUp?.showPopup("faild", "something went wrong, please try again")
-        }
-    };
-
-    const sendGroupRequests = async () => {
-        if (isSending) return; // optional extra guard
-        setIsSending(true);
-        const body: GroupNotifications = {
-            group_id: id,
-            requested_id: invited,
-            type: "invitation",
-        }
-        try {
-            const data = await SendJoinGroupRequest(body);
-            if (!data) {
-                setIsSending(true)
-            }
-            if (data.message) {
-                poopUp?.showPopup("success", data.message)
-            } else {
-                poopUp?.showPopup("faild", "something went wrong, please try again")
-            }
-
-        } catch (err) {
-            poopUp?.showPopup("faild", "something went wrong, please try again")
-
-        } finally {
-            setIsSending(true)
-        }
-    }
-
-    if (!groupInfo) return null;
-    return (
-        <div className="event-group-header">
-            <div className="group-info-card">
-                <div className="group-card-header">
-                    <div className="group-header-title">
-                        <MdGroups className="group-icon" />
-                        <span className="group-creation-date">{FormatDate(groupInfo.group.created_at)}</span>
-                        <div className="total-members">Members: {groupInfo.total_members}</div>
-                    </div>
-
-                    <div className="group-admin-card">
-                        <p className="admin-name">{groupInfo.author.firstname} {groupInfo.author.lastname}</p>
-                        {groupInfo.author.nickname && (
-                            <p className="admin-nickname">
-                                <MdVerifiedUser className="group-admin-icon" title="Group Admin" />
-                                @{groupInfo.author.nickname}
-                            </p>
-                        )}
-                    </div>
-                </div>
-
-                <div className="group-details">
-                    <p> {groupInfo.group.title}</p>
-                    <div className="event-group-description">{groupInfo.group.description}</div>
-                </div>
-
-                <div className="group-action-buttons">
-                    <button
-                        className="group-btn see-members"
-                        onClick={async () => {
-                            if (!showMembers) {
-                                await fetchMembers();
-                                setShowFolowers(false);
-                                setInvited((prev) => [])
-                                setIsSending(false)
-                            }
-                            setShowMembers((prev) => !prev);
-                        }}
-
-                    >
-                        {showMembers ? "Hide Members" : "See Members"}
-                    </button>
-
-                    <button
-                        className="group-btn invite-users"
-                        onClick={async () => {
-                            if (!showFolowers) {
-                                setInvited((prev) => prev = [])
-                                await fetchFolowers(); // fetch only when opening
-                                setShowMembers(false); // close members if opening followers
-                            }
-                            setShowFolowers((prev) => !prev); // toggle state
-                            setIsSending(false)
-                        }}
-
-                    >
-                        {showFolowers ? "Back" : "Invite Users"}
-                    </button>
-                </div>
-                {showMembers && (
-                    <div className="group-members-list">
-                        {!groupMembers ? (
-                            <p className="no-data">Unable to fetch group members.</p>
-                        ) : groupMembers.members.length === 0 ? (
-                            <p  className="no-data">No members in this group.</p>
-                        ) : (
-                            displayMembers()
-                        )}
-                    </div>
-                )}
-
-                {showFolowers && (
-                    <div className="group-members-list">
-                        {!followers || followers.length === 0 ? (
-                            <p  className="no-data">No followers available to invite.</p>
-                        ) : (
-                            displayFolowers()
-                        )}
-                    </div>
-                )}
-
-                {showFolowers && invited && invited.length > 0 && (
-                    <button
-                        className="group-btn send-invetation"
-                        onClick={async () => {
-                            await sendGroupRequests()
-                        }}
-                    >
-                        {isSending ? "Request Sent" : "Send"}
-                    </button>
-                )}
-            </div>
-        </div>
+  const handleToggleFollower = (id: number, checked: boolean) => {
+    setInvited((prev) =>
+      checked ? [...prev, id] : prev.filter((uid) => uid !== id)
     );
+  };
+
+  const displayMembers = () => {
+    return (
+      <div className="share-with-users">
+        <label className="share-with-label">Gorup members</label>
+        <ul className="user-checkbox-list">
+          {groupMembers &&
+            groupMembers.members.map((member, indx) => (
+              <li key={indx} className="event-member-item">
+                <PostHeader
+                  author={member.nickname}
+                  avatarUrl={member.avatar}
+                  firstname={member.firstname}
+                  lastname={member.lastname}
+                  createdAt=""
+                />
+              </li>
+            ))}
+        </ul>
+      </div>
+    );
+  };
+
+  const displayFolowers = () => {
+
+    return (
+      <div className="share-with-users">
+        <label className="share-with-label">Invite specific followers</label>
+        <ul className="user-checkbox-list">
+          {followers &&
+            followers.length > 0 &&
+            followers.map((f, index) => (
+              <li key={index} className="user-checkbox-item">
+                <label id="follower-checkbox-label">
+                  <PostHeader
+                    author={`${f.User?.firstname} ${f.User?.lastname}`}
+                    firstname={f.User?.firstname}
+                    lastname={f.User?.lastname}
+                    createdAt=""
+                    avatarUrl={f.User?.avatar}
+                  />
+                  <input
+                    type="checkbox"
+                    checked={invited.includes(f.User?.id)}
+                    onChange={(e) =>
+                      handleToggleFollower(f.User?.id, e.target.checked)
+                    }
+                    title={`Invite ${f.User?.firstname} ${f.User?.lastname}`}
+                    placeholder={`Select follower ${f.User?.firstname} ${f.User?.lastname}`}
+                    aria-label={`Invite ${f.User?.firstname} ${f.User?.lastname}`}
+                  />
+                </label>
+              </li>
+            ))}
+        </ul>
+      </div>
+    );
+  };
+
+  useEffect(() => {
+    const fetchGroup = async () => {
+      try {
+        const data = await GetGroup(id);
+        if (data.error === "not-found") {
+          router.push("/groups/joined/");
+          return;
+        }
+        setGroupInfo(data.data);
+      } catch (err) {
+        poopUp?.showPopup("faild", "something went wrong, please try again");
+      }
+    };
+
+    fetchGroup();
+  }, [id]);
+
+  const fetchMembers = async () => {
+    try {
+      const data = await GetGroupMembers(id);
+      setGroupMembers(data);
+    } catch (err) {
+      poopUp?.showPopup("faild", "something went wrong, please try again");
+    }
+  };
+
+  const fetchFolowers = async () => {
+    try {
+      const data = await GetUnrequestedFolowers(id);
+      setFollowers(data);
+    } catch (err) {
+      poopUp?.showPopup("faild", "something went wrong, please try again");
+    }
+  };
+
+  const sendGroupRequests = async () => {
+    if (isSending) return; // optional extra guard
+    setIsSending(true);
+    const body: GroupNotifications = {
+      group_id: id,
+      requested_id: invited,
+      type: "invitation",
+    };
+    try {
+      const data = await SendJoinGroupRequest(body);
+      if (!data) {
+        setIsSending(true);
+      }
+      if (data.message) {
+        poopUp?.showPopup("success", data.message);
+      } else {
+        poopUp?.showPopup("faild", "something went wrong, please try again");
+      }
+    } catch (err) {
+      poopUp?.showPopup("faild", "something went wrong, please try again");
+    } finally {
+      setIsSending(true);
+    }
+  };
+
+  if (!groupInfo) return null;
+  return (
+    <div className="event-group-header">
+      <div className="group-info-card">
+        <div className="group-card-header">
+          <div className="group-header-title">
+            <MdGroups className="group-icon" />
+            <span className="group-creation-date">
+              {FormatDate(groupInfo.group.created_at)}
+            </span>
+            <div className="total-members">
+              Members: {groupInfo.total_members}
+            </div>
+          </div>
+
+          <div className="group-admin-card">
+            <p className="admin-name">
+              {groupInfo.author.firstname} {groupInfo.author.lastname}
+            </p>
+            {groupInfo.author.nickname && (
+              <p className="admin-nickname">
+                <MdVerifiedUser
+                  className="group-admin-icon"
+                  title="Group Admin"
+                />
+                @{groupInfo.author.nickname}
+              </p>
+            )}
+          </div>
+        </div>
+
+        <div className="group-details">
+          <p> {groupInfo.group.title}</p>
+          <div className="event-group-description">
+            {groupInfo.group.description}
+          </div>
+        </div>
+
+        <div className="group-action-buttons">
+          <button
+            className="group-btn see-members"
+            onClick={async () => {
+              if (!showMembers) {
+                await fetchMembers();
+                setShowFolowers(false);
+                setInvited((prev) => []);
+                setIsSending(false);
+              }
+              setShowMembers((prev) => !prev);
+            }}
+          >
+            {showMembers ? "Hide Members" : "See Members"}
+          </button>
+
+          <button
+            className="group-btn invite-users"
+            onClick={async () => {
+              if (!showFolowers) {
+                setInvited((prev) => (prev = []));
+                await fetchFolowers(); // fetch only when opening
+                setShowMembers(false); // close members if opening followers
+              }
+              setShowFolowers((prev) => !prev); // toggle state
+              setIsSending(false);
+            }}
+          >
+            {showFolowers ? "Back" : "Invite Users"}
+          </button>
+        </div>
+        {showMembers && (
+          <div className="group-members-list">
+            {!groupMembers ? (
+              <p className="no-data">Unable to fetch group members.</p>
+            ) : groupMembers.members.length === 0 ? (
+              <p className="no-data">No members in this group.</p>
+            ) : (
+              displayMembers()
+            )}
+          </div>
+        )}
+
+        {showFolowers && (
+          <div className="group-members-list">
+            {!followers || followers.length === 0 ? (
+              <p className="no-data">No followers available to invite.</p>
+            ) : (
+              displayFolowers()
+            )}
+          </div>
+        )}
+
+        {showFolowers && invited && invited.length > 0 && (
+          <button
+            className="group-btn send-invetation"
+            onClick={async () => {
+              await sendGroupRequests();
+            }}
+          >
+            {isSending ? "Request Sent" : "Send"}
+          </button>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default GroupHeader;

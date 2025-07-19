ee5e71e38c466f7d33ad4d2dfc1cdf5c08308ccf
yelasri <elasriyoussef604@gmail.com>
2025-07-19 12:59:24 +0100

    last version

--- backend/pkg/db/temp/000010_comments_trigers.down.sql
+++
@@ -1,2 +0,0 @@
-DROP TRIGGER IF EXISTS increment_total_comments;
-
--- backend/pkg/db/temp/000010_comments_trigers.up .sql
+++
@@ -1,8 +0,0 @@
-
-CREATE TRIGGER increment_total_comments
-AFTER INSERT ON comments
-BEGIN
-  UPDATE posts
-  SET total_comments = total_comments + 1
-  WHERE id = NEW.post_id;
-END;
